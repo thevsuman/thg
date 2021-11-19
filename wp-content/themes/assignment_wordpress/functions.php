3fdfbd8f5ae719f92d752b25b6cb8ed96199e298
thevsuman-wwf <thevsuman@gmail.com>
2021-11-19 08:07:47 +0530

    chnages

--- wp-content/themes/assignment_wordpress/functions.php
+++ wp-content/themes/assignment_wordpress/functions.php
@@ -10,20 +10,6 @@ add_action( 'add_meta_boxes', 'thg_register_meta_boxes' );
 
 */
 
-function load_stylesheets()
-{
-    wp_register_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css',array(), false,all );
-    wp_enqueue_style( 'bootstrap');
-}
-add_action( 'wp_enqueue_scripts', 'load_stylesheets');
-
-
-function loadjs(){
-    wp_register_script('customjs', get_template_directory_uri() .'/js/scripts.js', '', 1, true );
-    wp_enqueue_script('wp_enqueue_scripts','loadjs');
-}
-add_action('wp_enqueue_scripts','loadjs');
-
 function cptui_register_my_cpts() {
 
 	/**
