02aca425af6c38bc87f0f34306d172cd5989de0b
thevsuman-wwf <72057958+thevsuman-wwf@users.noreply.github.com>
2021-11-23 04:18:15 +0530

    fixes

--- wp-content/themes/assignment_wordpress/functions.php
+++ wp-content/themes/assignment_wordpress/functions.php
@@ -2,9 +2,9 @@
 
 function load_stylesheets()
 {
-    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, all);
+    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false);
     wp_enqueue_style('bootstrap');
-	wp_register_style('style-css', get_template_directory_uri() . '/css/custom-style.css', array(), false, all);
+    wp_register_style('style-css', get_template_directory_uri() . '/css/custom-style.css', array(), false);
     wp_enqueue_style('style-css');
 }
 add_action('wp_enqueue_scripts', 'load_stylesheets');
@@ -12,8 +12,8 @@ add_action('wp_enqueue_scripts', 'load_stylesheets');
 
 function collectiveray_theme_scripts_function()
 {
-	wp_enqueue_script('jquery-js','https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js', array(), null, false );		
-	wp_enqueue_script('js-file', get_template_directory_uri() . '/js/scripts.js');
+    wp_enqueue_script('jquery-js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js', array(), null, false);
+    wp_enqueue_script('js-file', get_template_directory_uri() . '/js/scripts.js');
     wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js');
 }
   add_action('wp_enqueue_scripts', 'collectiveray_theme_scripts_function');
@@ -58,51 +58,54 @@ add_filter('wpcf7_posted_data', 'save_posted_data');
 
 /*Ajax Call
 */
-function add_ajax_scripts() {
-    wp_enqueue_script( 'ajaxcalls', get_template_directory_uri() . '/js/ajax-calls.js', array(), '1.0.0', true );
+function add_ajax_scripts()
+{
+    wp_enqueue_script('ajaxcalls', get_template_directory_uri() . '/js/ajax-calls.js', array(), '1.0.0', true);
 
-    wp_localize_script( 'ajaxcalls', 'ajax_object', array(
-        'ajaxurl' => admin_url( 'admin-ajax.php' ),
-        'ajaxnonce' => wp_create_nonce( 'ajax_post_validation' )
-    ) );
+    wp_localize_script('ajaxcalls', 'ajax_object', array(
+        'ajaxurl' => admin_url('admin-ajax.php'),
+        'ajaxnonce' => wp_create_nonce('ajax_post_validation')
+    ));
 }
 
-add_action( 'wp_enqueue_scripts', 'add_ajax_scripts' );
+add_action('wp_enqueue_scripts', 'add_ajax_scripts');
 
 
-function custom_update_post() {
+function custom_update_post()
+{
     $post_id = $_POST['post_id'];
-	
-    update_post_meta( $post_id, 'first_name', $_POST['first_name']);
-	update_post_meta( $post_id, 'last_name', $_POST['last_name'] );
-    update_post_meta( $post_id, 'email', $_POST['email'] );
-    update_post_meta( $post_id, 'phone', $_POST['phone'] );
-    update_post_meta( $post_id, 'address', $_POST['address'] );
-    update_post_meta( $post_id, 'city', $_POST['city'] );
+    
+    update_post_meta($post_id, 'first_name', $_POST['first_name']);
+    update_post_meta($post_id, 'last_name', $_POST['last_name']);
+    update_post_meta($post_id, 'email', $_POST['email']);
+    update_post_meta($post_id, 'phone', $_POST['phone']);
+    update_post_meta($post_id, 'address', $_POST['address']);
+    update_post_meta($post_id, 'city', $_POST['city']);
     
     wp_die();
 }
 
-add_action( 'wp_ajax_custom_update_post', 'custom_update_post' );
+add_action('wp_ajax_custom_update_post', 'custom_update_post');
 
 //Email Validation already exits or not
-add_filter( 'wpcf7_validate_email*', 'custom_email_validation_filter', 50, 2 );
+add_filter('wpcf7_validate_email*', 'custom_email_validation_filter', 50, 2);
 
-function custom_email_validation_filter( $result, $tag ) {
-if ( 'your-email' == $tag->name ) {
-$your_email = isset( $_POST['your-email'] ) ? trim( $_POST['your-email'] ) : '';
+function custom_email_validation_filter($result, $tag)
+{
+    if ('your-email' == $tag->name) {
+        $your_email = isset($_POST['your-email']) ? trim($_POST['your-email']) : '';
 
-global $wpdb; 
+        global $wpdb;
 
-$cf7table = $wpdb->prefix.'db7_forms';
+        $cf7table = $wpdb->prefix.'db7_forms';
 
-$db = $wpdb->get_results ( "SELECT * FROM $cf7table WHERE `form_value` LIKE '%".$your_email."%' "); 
-//echo $wpdb->num_rows;
-print_r($db);
-if ( $wpdb->num_rows > 0) {
-$result->invalidate( $tag, "The email is already exist." );
-}
-}
+        $db = $wpdb->get_results("SELECT * FROM $cf7table WHERE `form_value` LIKE '%".$your_email."%' ");
+        //echo $wpdb->num_rows;
+        print_r($db);
+        if ($wpdb->num_rows > 0) {
+            $result->invalidate($tag, "The email is already exist.");
+        }
+    }
 
-return $result;
+    return $result;
 }
--- wp-content/themes/assignment_wordpress/page.php
+++ wp-content/themes/assignment_wordpress/page.php
@@ -2,58 +2,82 @@
 
 <body <?php body_class();?>>
 
-<h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-
-<?php echo do_shortcode( '[contact-form-7 id="6" title="Client form"]' ); ?>
-
-
+   <!-- <h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+-->
+    <?php echo do_shortcode( '[contact-form-7 id="6" title="Client form"]' ); ?>
 
 
-<?php
+
+
+    <?php
 // Get the 'Profiles' post type
 $args = array(
     'post_type' => 'client',
 );
 $loop = new WP_Query($args);
-echo '<table>';
-echo '<tr>';
+echo '<div class="container row-data">';
 while($loop->have_posts()): $loop->the_post();
 
-echo '<td>';
+echo '<div class="row">';
+
+/*
+echo '<div class="col-md col-sm">';
 the_title();
-echo '</td>';
+echo '</div>';
+*/
+echo '<div class="col-md col-sm cel">';
+$first_name = get_post_meta($post->ID,'first_name',true);
+//echo $value;
+?> <input type='text' class='<?php echo $post->ID; ?>first_name' value=<?php echo $first_name; ?> name='first_name' /> <?php
+echo '</div>';
 
-echo '<td>';
-$value = get_post_meta($post->ID,'first_name',true);
-echo $value;
-echo '</td>';
+echo '<div class="col-md col-sm cel">';
+$last_name = get_post_meta($post->ID,'last_name',true);
+//echo $value;
+?> <input type='text' class='<?php echo $post->ID; ?>last_name' value=<?php echo $last_name; ?> name='last_name' /> <?php
 
-echo '<td>';
-$value = get_post_meta($post->ID,'last_name',true);
-echo $value;
-echo '</td>';
+echo '</div>';
 
-echo '<td>';
-$value = get_post_meta($post->ID,'email',true);
-echo $value;
-echo '</td>';
+echo '<div class="col-md col-sm cel">';
+$email = get_post_meta($post->ID,'email',true);
+//echo $value;
+?> <input type='text' class='<?php echo $post->ID; ?>email' value=<?php echo $email; ?> name='email' /> <?php
 
-echo '<td>';
-$value = get_post_meta($post->ID,'phone',true);
-echo $value;
-echo '</td>';
+echo '</div>';
 
-echo '<td>';
-$value = get_post_meta($post->ID,'address',true);
-echo $value;
-echo '</td>';
+echo '<div class="col-md col-sm cel">';
+$phone = get_post_meta($post->ID,'phone',true);
+//echo $value;
+?> <input type='text' class='<?php echo $post->ID; ?>phone' value=<?php echo $phone; ?> name='phone' /> <?php
 
-echo '</tr>';
+echo '</div>';
+
+echo '<div class="col-md col-sm cel">';
+$address = get_post_meta($post->ID,'address',true);
+?> <input type='text' class='<?php echo $post->ID; ?>address' value=<?php echo $address; ?> name='address' /> <?php
+
+//echo $value;
+echo '</div>';
+
+echo '<div class="col-md col-sm cel">';
+$city = get_post_meta($post->ID,'city',true);
+?> <input type='text' class='<?php echo $post->ID; ?>city' value=<?php echo $city[0]; ?> name='city' /> <?php
+
+echo '</div>';
+
+
+echo '<div class="col-md col-sm cel">';
+?>
+<a onclick="return confirm('Are you SURE you want to delete this post?')" href="<?php echo get_delete_post_link( $post->ID ) ?>">Delete</a>
+<a id="<?php echo get_the_ID(); ?>" class="your_class_to_update"> Update</a>
+<?php
+echo '</div>';
+echo '</div>';
 endwhile;
-echo '</table>';
+
 wp_reset_query();
 ?>
 
-<body>
+    <body>
 
-<?php get_footer(); ?>
+        <?php get_footer(); ?>
